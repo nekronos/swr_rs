7338888d1d0345f4c56d730e8d15f56e88151394
nekronos <lende.vegard@gmail.com>
2016-12-09 23:07:06 +0100

    Matrix cleanup

--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -86,24 +86,25 @@ impl Matrix4 {
     }
 
     pub fn identity() -> Matrix4 {
-        Matrix4::new(1.0,
-                     0.0,
-                     0.0,
-                     0.0,
-                     0.0,
-                     1.0,
-                     0.0,
-                     0.0,
-                     0.0,
-                     0.0,
-                     1.0,
-                     0.0,
-                     0.0,
-                     0.0,
-                     0.0,
-                     1.0)
+        Matrix4 {
+            m11: 1.0,
+            m12: 0.0,
+            m13: 0.0,
+            m14: 0.0,
+            m21: 0.0,
+            m22: 1.0,
+            m23: 0.0,
+            m24: 0.0,
+            m31: 0.0,
+            m32: 0.0,
+            m33: 1.0,
+            m34: 0.0,
+            m41: 0.0,
+            m42: 0.0,
+            m43: 0.0,
+            m44: 1.0,
+        }
     }
-
 
     pub fn look_at_lh(eye: Vector3, target: Vector3, up: Vector3) -> Matrix4 {
         let zaxis = (target - eye).normalize();
@@ -193,11 +194,12 @@ impl Matrix4 {
     }
 
     pub fn translation(offset: Vector3) -> Matrix4 {
-        let mut m = Matrix4::identity();
-        m.m41 = offset.x;
-        m.m42 = offset.y;
-        m.m43 = offset.z;
-        m
+        Matrix4 {
+            m41: offset.x,
+            m42: offset.y,
+            m43: offset.z,
+            ..Matrix4::identity()
+        }
     }
 }
 
